ed9d5a6f887a43fee849ba62d4136a4d1c0ed637
Julio <juliosantosjob>
2023-06-22 14:37:10 -0300

    Ajusts

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [['html', { open: 'never' }]],
-  timeout: 3000,
+  timeout: 3 * 1000,
 
   use: {
     extraHTTPHeaders: {
